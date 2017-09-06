07362bb0832264365dd0f2b1838af8af238988f9
zswenjin <zswenjin.li@gmail.com>
2017-09-06 12:42:39 +1000

    Initial Commit

---
+++ TriviaQuestions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TriviaQuestions
+//
+//  Created by WENJIN LI on 2017/9/6.
+//  Copyright © 2017年 WENJIN LI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
