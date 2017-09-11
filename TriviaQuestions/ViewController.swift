3ca782c7901947dd129f71669fca9a57f7212222
zswenjin <zswenjin.li@gmail.com>
2017-09-11 14:57:07 +1000

    create func for "QuestionsProvider"

--- TriviaQuestions/ViewController.swift
+++ TriviaQuestions/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion: Int = 0
+    //var gamesound: SystemSoundID = 0
+    
+    
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var answerA: UIButton!
+    @IBOutlet weak var answerB: UIButton!
+    @IBOutlet weak var answerC: UIButton!
+    @IBOutlet weak var answerD: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +36,7 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
 }
 
