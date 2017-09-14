53a79434e73803ec353533587aa032c2044dc77d
zswenjin <zswenjin.li@gmail.com>
2017-09-14 12:11:45 +1000

    create func for displayQuestion()

--- TriviaQuestions/ViewController.swift
+++ TriviaQuestions/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     let questionsPerRound = 4
     var questionsAsked = 0
     var correctQuestions = 0
-    var answer: String = " "
+    var correctAnswer: String = " "
     var indexOfSelectedQuestion: Int = 0
     var soundForCorrectAnswer: SystemSoundID = 0
     var soundForWrongAnswer: SystemSoundID = 0
@@ -27,6 +27,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerB: UIButton!
     @IBOutlet weak var answerC: UIButton!
     @IBOutlet weak var answerD: UIButton!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var feedbackField: UILabel!
+    @IBOutlet weak var nextQuestion: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
     
     
@@ -34,6 +37,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayQuestion()
     }
 
     
@@ -42,9 +46,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func displayQuestion() {
+        let questions = getRandomQuestion()
+        questionField.text = questions.question
+        correctAnswer = questions.correctAnswer
+        
+        //set value for the four answer bottons accordingly
+        answerA.setTitle(questions.choiceA, for: .normal)
+        answerB.setTitle(questions.choiceB, for: .normal)
+        answerC.setTitle(questions.choiceC, for: .normal)
+        answerD.setTitle(questions.choiceD, for: .normal)
+        
+        //hide botton
+        playAgainButton.isHidden = true
+        feedbackField.isHidden = true
+        feedbackLabel.isHidden = true
+    }
     
     
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        //Increment the questions asked counter
+        questionsAsked += 1
     
+        
+        
+    }
+    
+    
+
     
 
 
